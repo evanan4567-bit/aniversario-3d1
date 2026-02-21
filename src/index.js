75e858816c4050f92e89ec67f7b93fe13d7b131e
Ivan <evanan4567@gmail.com>
2026-02-21 18:04:17 -0500

    Clean index.js without service worker

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// 👇 agrega esta línea
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// 👇 y agrega esta línea
-serviceWorkerRegistration.register();
